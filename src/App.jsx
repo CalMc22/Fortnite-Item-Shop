c1590b86848aadb1d071ec759b27886e0d02274b
Caleb Krysos <CalMc22>
2024-01-04 14:54:38 -0500

    api function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
-function App() {
+function App(){ {
+  
+  let [value, setValue] = useState({})
+
+  async function getData() {
+  let promise = await fetch ('https://fortnite-api.com/v1/map')
+  let data = await promise.json()
+  setValue(data)
+  console.log(value)
+}
+useEffect (() => {
+  getData()
+  } , [])
+
+}
+
+
+
+
 
   return (
     <>
-      <div>
-Fortnite Map is on Netlify!
-      </div>
+          <div>
+            hi
+          </div>
     </>
   )
-}
+  }
 
 export default App
