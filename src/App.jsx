686d5b603f1612cf8680f4adfd052d08d154336b
Caleb Krysos <CalMc22>
2024-01-05 13:00:48 -0500

    map over array of featured icon images and display on screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,50 @@ function App() {
 
   let [fnshop, setFnshop] = useState(null)
 
-  async function getData() {
-    try {
-      let promise = await fetch('https://fortnite-api.com/v2/shop/br')
-      let data = await promise.json()
-      setFnshop(data)
-    } catch {
-      console.log(error)
+  useEffect(() => {
+    async function getData() {
+      try {
+        let promise = await fetch('https://fortnite-api.com/v2/shop/br')
+        let data = await promise.json()
+        setFnshop(data)
+      } catch {
+        console.log(error)
+      }
+
     }
 
-  }
-
-  useEffect(() => {
     getData()
   }, [])
 
   console.log(fnshop)
 
+  let shopItemArray = fnshop ? fnshop.data.featured.entries : null
 
+  console.log(shopItemArray)
 
 
 
   return (
-    <>
+    <div className='container'>
       {fnshop ?
         <div>
-          <h1>
+          <h1 className='title'>
             <img src={fnshop.data.vbuckIcon} height={25} />
             Fortnite Item Shop as of {fnshop.data.date}
             <img src={fnshop.data.vbuckIcon} height={25} />
           </h1>
 
-          <div>
-            <img src={fnshop.data.featured.entries[0].items[0].images.icon} height={200} />
-            <img src={fnshop.data.featured.entries[1].items[0].images.icon} height={200} />
-            <img src={fnshop.data.featured.entries[2].items[0].images.icon} height={200} />
+          <div className='shop'>
+            {shopItemArray.map((entries, index) => (
+              <img className='shopEntry' key={index} src={entries.items[0].images.featured} />
+            ))}
           </div>
         </div>
         :
         null
 
       }
-    </>
+    </div>
   )
 }
 
