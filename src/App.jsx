64602c0793f538c800c9a12b4fde9a0b0bd76ffe
Caleb Krysos <CalMc22>
2024-01-08 16:54:04 -0500

    deleted bundle tab and replaced with a working lego tab

--- src/App.jsx
+++ src/App.jsx
@@ -26,21 +26,33 @@ function App() {
   // checks if fnshop exists, if so, set variable to entries array. If not, return null
   let shopFeaturedItemArray = fnshop ? fnshop.data.featured.entries : null
 
+  /*
+    // console logs for testing targeting the api
+    if (fnshop) {
+      console.log('Featured below')
+      console.log(shopFeaturedItemArray)
+  
+      console.log('Bundles below')
+      shopFeaturedItemArray.map((entries, index) => (
+        console.log(entries.bundle)
+      ))
+      // console.log(shopFeaturedItemArray[0].bundle.image)
+    } else {
+      console.log('If statement failed')
+    }
+    */
 
-  // console logs for testing targeting the api
-  if (fnshop) {
-    console.log('Featured below')
-    console.log(shopFeaturedItemArray)
-
-    console.log('Bundles below')
-    shopFeaturedItemArray.map((entries, index) => (
-      console.log(entries[index])
-    ))
-    // console.log(shopFeaturedItemArray[0].bundle.image)
-  } else {
-    console.log('If statement failed')
-  }
-
+  // function getBundle() {
+  //   shopFeaturedItemArray.map(function(entries, index) {
+  //     if (entries.banner = null) {
+  //       console.log('getBundle null')
+  //       return <div>No Bundle</div>
+  //     } else {
+  //       console.log('getBundle image')
+  //       return <img className='shopEntry' key={index + 1000} src={entries.items[0].images.featured} />
+  //     }
+  //   })
+  //   }
 
 
   return (
@@ -52,7 +64,7 @@ function App() {
             <img className='vbuckIcon' src={fnshop.data.vbuckIcon} />
             {/* targets the current shop date */}
             Fortnite Item Shop as of {fnshop.data.date}
-            <img className='vbuckIcon'src={fnshop.data.vbuckIcon} />
+            <img className='vbuckIcon' src={fnshop.data.vbuckIcon} />
           </h1>
           <h2 className='shopTab'>FEATURED COSMETICS</h2>
           <div className='shop'>
@@ -61,17 +73,20 @@ function App() {
               <img className='shopEntry' key={index} src={entries.items[0].images.icon} />
             ))}
           </div>
-          <h3 className='shopTab'>BUNDLES</h3>
+          <h3 className='shopTab'>LEGO</h3>
           <div className='shop'>
-            {/* {shopFeaturedItemArray.map((entries, index) => (
-                  <img className='shopEntry' key={index + 1000} src={entries[0].bundle.image || null} />
-                ))} */}
+            {/* maps over the shopFeaturedItemArray variable, checks in lego exists, if so, renders each shop entry small lego icon */}
+            {shopFeaturedItemArray.map((entries, index) => (
+              <img className='shopEntry' key={index + 1000}
+                src={entries.items[0].images.lego
+                  ? entries.items[0].images.lego.small
+                  : entries.items[0].images.lego} />
+            ))}
+
           </div>
         </div>
         // if fnshop DOES NOT exist, render loading div
-        :
-        <div className='loading'>Loading...</div>
-
+        : <div className='loading'>Loading...</div>
       }
     </div>
   )
