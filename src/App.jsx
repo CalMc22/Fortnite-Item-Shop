f45c36f342f5607256229c00852ebfd2759f3aa4
Caleb Krysos <CalMc22>
2024-01-08 14:35:26 -0500

    added loading when getting data and added comments in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,42 +23,54 @@ function App() {
 
   console.log(fnshop)
 
+  // checks if fnshop exists, if so, set variable to entries array. If not, return null
   let shopFeaturedItemArray = fnshop ? fnshop.data.featured.entries : null
 
-//   if (fnshop){
-//   console.log('Featured below')
-//   console.log(shopFeaturedItemArray)
 
-//   console.log('Bundles below')
-//   console.log(shopFeaturedItemArray[0].bundle.image)
-// } else {
-//   console.log('If statement failed')
-// }
+  // console logs for testing targeting the api
+  if (fnshop){
+  console.log('Featured below')
+  console.log(shopFeaturedItemArray)
+
+  console.log('Bundles below')
+  shopFeaturedItemArray.map((entries, index) => (
+  console.log(entries[index])
+  ))
+  // console.log(shopFeaturedItemArray[0].bundle.image)
+} else {
+  console.log('If statement failed')
+}
+
+
 
   return (
     <div className='container'>
+      {/* check if fnshop exists, if so, render the following */}
       {fnshop ?
         <div>
           <h1 className='title'>
             <img src={fnshop.data.vbuckIcon} height={40} />
+            {/* targets the current shop date */}
             Fortnite Item Shop as of {fnshop.data.date}
             <img src={fnshop.data.vbuckIcon} height={40} />
           </h1>
           <h2 className='shopTab'>FEATURED COSMETICS</h2>
           <div className='shop'>
+            {/* maps over the shopFeaturedItemArray variable and renders each shop entry icon */}
             {shopFeaturedItemArray.map((entries, index) => (
               <img className='shopEntry' key={index} src={entries.items[0].images.icon} />
               ))}
           </div>
-          {/* <h3 className='shopTab'>BUNDLES</h3>
+          <h3 className='shopTab'>BUNDLES</h3>
               <div className='shop'>
-                {shopFeaturedItemArray.map((entries, index) => (
-                  <img className='shopEntry' key={index} src={entries.bundle} />
-                ))}
-              </div> */}
+                {/* {shopFeaturedItemArray.map((entries, index) => (
+                  <img className='shopEntry' key={index + 1000} src={entries[0].bundle.image || null} />
+                ))} */}
+              </div>
         </div>
+        // if fnshop DOES NOT exist, render loading div
         :
-        null
+        <div className='loading'>Loading...</div>
 
       }
     </div>
