92e4fe1f7edbe443fc380fa0ea12c91185382edc
Caleb Krysos <CalMc22>
2024-02-01 00:30:51 -0500

    changed baseURL to work with VITE_API or localhost8080

--- frontend/Api.js
+++ frontend/Api.js
@@ -1,2 +1,2 @@
-const baseURL = import.meta.env.PROD ? import.meta.env.VITE_API : 'https://fn-item-shop.onrender.com'
+const baseURL = import.meta.env.VITE_API ? import.meta.env.VITE_API : 'https://localhost:8080'
     export default baseURL
