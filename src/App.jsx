8408cdae9fbcc534a3e14dea324bcd19d51f3741
Cal 22 <CalMc22>
2025-02-09 14:14:47 -0500

    fixed shop date and icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import './App.css'
 
 function App() {
 
-    let [fnshop, setFnshop] = useState(null)
+
+    let [fnshop, setfnshop] = useState(null)
 
     useEffect(() => {
         async function getData() {
             try {
-                let promise = await fetch('https://fortnite-api.com/v2/shop/br/combined')
+                let promise = await fetch('https://fortnite-api.com/v2/shop')
                 let data = await promise.json()
-                setFnshop(data)
+                setfnshop(data)
             } catch {
                 console.log(error)
             }
@@ -22,26 +23,49 @@ function App() {
 
 
     // checks if fnshop exists, if so, set variable to entries array. If not, return null
-    let shopFeaturedItemArray = fnshop ? fnshop.data.featured.entries : null
+    let shopFeaturedItemArray = fnshop ? fnshop.data.entries : null
 
-    /*
+
     // console logs for testing targeting the api
-  
-    // console.log(fnshop)
-  
+
+    console.log(fnshop)
+
     if (fnshop) {
         console.log('Featured below')
         console.log(shopFeaturedItemArray)
-    
-        console.log('Bundles below')
-        shopFeaturedItemArray.map((entries, index) => (
-          console.log(entries.bundle)
-        ))
+
+        console.log('fnshop below')
+        console.log(fnshop)
+
+        console.log('vbuck Icon below')
+        console.log(fnshop.data.vbuckIcon)
+
+        console.log('fnshop.data.entries below')
+        console.log(fnshop.data.entries[1 + 300])
+
+
+        // console.log('Bundles below')
+        // shopFeaturedItemArray.map((entries, index) => (
+        //   console.log(entries.bundle)
+        // ))
         // console.log(shopFeaturedItemArray[0].bundle.image)
-      } else {
+    } else {
         console.log('If statement failed')
-      }
-      */
+    }
+
+
+    
+// <h3 className='shopTab'>LEGO STYLES</h3>
+//                         <div className='shop'>
+//                             {/* maps over the shopFeaturedItemArray variable, checks in lego exists, if so, renders each shop entry small lego icon */}
+//                             {shopFeaturedItemArray.map((entries, index) => (
+//                                 <img className='shopEntry' key={index + 1000}
+//                                     src={entries.items[0].images.lego
+//                                         ? entries.items[0].images.lego.small
+//                                         : entries.items[0].images.lego} />
+//                             ))}
+//                         </div>
+
 
     return (
         <>
@@ -59,20 +83,9 @@ function App() {
                         <div className='shop'>
                             {/* maps over the shopFeaturedItemArray variable and renders each shop entry icon */}
                             {shopFeaturedItemArray.map((entries, index) => (
-                                <img className='shopEntry' key={index} src={entries.items[0].images.icon} />
+                                <div className='shopEntry' key={index} src={entries[index]} />
                             ))}
                         </div>
-                        <h3 className='shopTab'>LEGO STYLES</h3>
-                        <div className='shop'>
-                            {/* maps over the shopFeaturedItemArray variable, checks in lego exists, if so, renders each shop entry small lego icon */}
-                            {shopFeaturedItemArray.map((entries, index) => (
-                                <img className='shopEntry' key={index + 1000}
-                                    src={entries.items[0].images.lego
-                                        ? entries.items[0].images.lego.small
-                                        : entries.items[0].images.lego} />
-                            ))}
-                        </div>
-
                     </div>
                     // if fnshop DOES NOT exist, render loading div
                     : <div className='loading'>Loading...</div>
